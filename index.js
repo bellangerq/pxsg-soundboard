a19ecb7a0a102c95894f8029a9f615ef6ab2c4eb
Quentin Bellanger <hello@quentin-bellanger.com>
2018-05-11 20:35:59 +0200

    [+] Init app

---
+++ index.js
@@ -0,0 +1,49 @@
+const discord = require('discord.js')
+const bot = new discord.Client()
+let isReady = true
+
+/*
+const sounds = [
+  {
+    command: 'philippe',
+    file: 'philippe.mp3'
+  },
+  {
+    command: 'nanananana',
+    file: 'nanananana.mp3'
+  },
+  ...
+]
+*/
+
+bot.login('NDQ0NTQ1OTc1NTA4NTMzMjQ4.DddjZQ.OQPnvVlKez6-bhfA2BtPxWfrq5s')
+
+bot.on('message', message => {
+  if (message.content === '!philippe' && isReady) {
+    isReady = false
+    const voiceChannel = message.member.voiceChannel
+    voiceChannel.join()
+    .then(connection => {
+      const dispatcher = connection.playFile('./sounds/philippe.mp3')
+      dispatcher.on('end', end => {
+        voiceChannel.leave()
+      })
+    })
+    isReady = true
+  }
+})
+
+bot.on('message', message => {
+  if (message.content === '!nanananana' && isReady) {
+    isReady = false
+    const voiceChannel = message.member.voiceChannel
+    voiceChannel.join()
+    .then(connection => {
+      const dispatcher = connection.playFile('./sounds/nanananana.mp3')
+      dispatcher.on('end', end => {
+        voiceChannel.leave()
+      })
+    })
+    isReady = true
+  }
+})
---
+++ sounds/README.md
@@ -0,0 +1,13 @@
+# PXSG soundboard
+
+Another soundboard for Discord. But this one is the funniest.
+
+## Development
+
+Just run `npm run start` to launch local app. The bot will login to Discord. Then you can type the different commands.
+
+## To do
+- [ ] Create sounds array
+- [ ] Create global function to play sound
+- [ ] Create `!help` command
+- [ ] Create `!list` command
